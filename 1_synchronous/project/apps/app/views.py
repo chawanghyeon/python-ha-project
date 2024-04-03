ba7ac28dd07255b1828351ee95a3e06981cccecf
chawanghyeon <chawanghyeon@gmail.com>
2024-04-03 23:59:46 +0900

    feature: 모델 및 뷰 프로토타입 작성

--- 1_synchronous/project/apps/app/views.py
+++ 1_synchronous/project/apps/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class BookView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class EncodingView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
