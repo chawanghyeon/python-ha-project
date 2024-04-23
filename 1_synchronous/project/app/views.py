af649b73db168520073e1f6f9b4e0ce8f89edc2f
chawanghyeon <chawanghyeon@gmail.com>
2024-04-23 23:40:54 +0900

    feature: encoding api 개발

--- 1_synchronous/project/app/views.py
+++ 1_synchronous/project/app/views.py
@@ -2,6 +2,9 @@ from django.views import View
 from django.http import JsonResponse
 from .models import Book
 import onnxruntime as ort
+from PIL import Image
+import numpy as np
+import base64
 
 
 class BookView(View):
@@ -18,15 +21,19 @@ class EncodingView(View):
         pass
 
     def post(self, request):
-        image = request.FILES['image']
+        image_base64 = request.FILES['image']
+        image_width = request.POST['width']
+        image_height = request.POST['height']
         # Load the model
-        model = ort.InferenceSession('../../../resnet50-v2-7.onnx')
-        # Do some processing
-        output = model.run(image)
-        # Encode the output
-        encoded_image = output.encode('base64')
-        return JsonResponse({'encoded_image': encoded_image})
-        
-        
+        image_bytes_decoded = base64.b64decode(image_base64)
+        image = Image.frombytes("RGB", (image_width, image_height), image_bytes_decoded)
+        image = image.resize((224, 224))
+        image = np.array(image, dtype=np.float32)
+        image = image.transpose((2, 0, 1))
+        image = np.expand_dims(image, axis=0)
 
+        model = ort.InferenceSession("resnet.onnx")
+        output_bytes = model.run(None, {'data': image})[0].tobytes()
+        output_base64 = base64.b64encode(output_bytes).decode('utf-8')
 
+        return JsonResponse({'encoded_image': output_base64})
